3153dd1617c921961e6b8e22a7091d1b399e321a
Kalyan A <kalyan.shelf@gmail.com>
2024-09-03 08:26:27 +1000

    setting app

--- apps/settings-app/next.config.js
+++ apps/settings-app/next.config.js
@@ -13,30 +13,12 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
-  async rewrites() {
-    return [
-      {
-        source: '/dashboard/:slug*',
-        destination: 'http://localhost:3031/dashboard/:slug*',
-      },
-      {
-        source: '/@fs/:slug*',
-        destination: 'http://localhost:3031/dashboard/@fs/:slug*',
-      },
-      {
-        source: '/messages/:slug*',
-        destination: 'http://localhost:3032/messages/:slug*',
-      },
-      {
-        source: '/settings/:slug*',
-        destination: 'http://localhost:3033/settings/:slug*',
-      },
-    ];
-  },
+  basePath: '/settings',
   webpack: (config, options) => {
     config.plugins.push(
       new NextFederationPlugin({
-        name: 'home-app',
+        remoteType: 'module',
+        name: 'settings-app',
         filename: 'remoteEntry.js',
         remotes: {
           'shared-app': `promise import("http://localhost:4300/assets/remoteEntry.js")`,
